b2179b1842643f63ff85851cf1a46cfbe462200b
Yullia Kravchuk <ljuljek554@gmail.com>
2024-07-04 16:00:25 +0300

    test

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-'use strict'
+"use strict";
 
-const btnLucky = document.querySelector('.btn-lucky');
-const logo = document.querySelector('.logo');
-btnLucky.addEventListener('click', onBtnClick);
+const btnBrands = document.querySelector(".slogan-btn-brands");
+const btnAgency = document.querySelector(".slogan-btn-agency");
+const btn = document.querySelector(".slogan-wrap-btn");
+const tittleWord = document.querySelector(".tittle-word");
+btn.addEventListener("click", onBtnClick);
 
-function onBtnClick() {
-   logo.alt='Oddity'
-   logo.src='https://image.cnbcfm.com/api/v1/image/107200429-1677524943053-Screen_Shot_2022-09-15_at_115135_AM.png?v=1689776971&w=740&h=416&ffmt=webp&vtcrop=y'
+function onBtnClick(e) {
+  const btnActive = e.target;
+  if (btnActive === btnBrands) {
+    btnBrands.classList.add("is-active");
+    btnAgency.classList.remove("is-active");
+    tittleWord.textContent = "brands";
+  } else {
+    btnBrands.classList.remove("is-active");
+    btnAgency.classList.add("is-active");
+    tittleWord.textContent = "agency";
+  }
 }
